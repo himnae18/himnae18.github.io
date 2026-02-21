a34966a92d8476cdec2d2595f7d32d3dc6211c77
himnae18 <himnae18@gmail.com>
2026-02-21 16:29:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ window.onbeforeunload = () => {
   let sec = Math.floor((Date.now() - start) / 1000);
   localStorage.setItem("totalTime", total + sec);
 };
+
+let playlist = [
+  "songs/lemon.mp3",
+  "songs/yoru.mp3",
+  "songs/sparkle.mp3"
+];
+
+let current = 0;
+let player = document.getElementById("player");
+
+// 노래 클릭 재생
+function playSong(i) {
+  current = i;
+  player.src = playlist[current];
+  player.play();
+}
+
+// 노래 끝나면 자동 다음곡
+player.addEventListener("ended", () => {
+  current++;
+  if (current >= playlist.length) current = 0; // 반복
+  player.src = playlist[current];
+  player.play();
+});
