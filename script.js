a0dcbf5cab60297968aab7dd268747370e97fc9f
himnae18 <himnae18@gmail.com>
2026-02-21 17:30:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function addSong() {
-  const title = prompt("노래 제목?");
-  const youtube = prompt("유튜브 ID?");
-  const mr = prompt("MR 파일 이름?");
-  const score = prompt("악보 파일 이름?");
+let songs = JSON.parse(localStorage.getItem("jpBright")) || [];
 
-  const div = document.createElement("div");
-  div.innerHTML = `
-  <h3>${title}</h3>
-  <iframe width="400" src="https://www.youtube.com/embed/${youtube}"></iframe>
-  <audio controls src="../songs/${mr}"></audio>
-  <a href="../score/${score}">악보</a>
-  <hr>
-  `;
-  document.getElementById("songList").appendChild(div);
+function addSong(){
+  let title = document.getElementById("title").value;
+  let yt = document.getElementById("yt").value;
+
+  songs.push({title, yt});
+  localStorage.setItem("jpBright", JSON.stringify(songs));
+  location.reload();
+}
+
+window.onload = () => {
+  songs.forEach(s=>{
+    let id = s.yt.split("v=")[1];
+    document.getElementById("list").innerHTML += `
+      <h3>${s.title}</h3>
+      <iframe width="300" src="https://www.youtube.com/embed/${id}"></iframe>
+      <hr>
+    `;
+  });
 }
