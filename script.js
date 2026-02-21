c5b34ea8831c569998dfdfb420a1b491c72459ce
himnae18 <himnae18@gmail.com>
2026-02-21 16:26:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let total = Number(localStorage.getItem("totalTime")) || 0;
+let start = Date.now();
 
+setInterval(() => {
+  let sec = Math.floor((Date.now() - start) / 1000);
+  document.getElementById("time") &&
+    (document.getElementById("time").innerText =
+      "총 사이트 머문 시간: " + (total + sec) + "초");
+}, 1000);
+
+window.onbeforeunload = () => {
+  let sec = Math.floor((Date.now() - start) / 1000);
+  localStorage.setItem("totalTime", total + sec);
+};
