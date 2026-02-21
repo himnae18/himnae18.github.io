62a1ffcb728d47f07f317c0e663f0d5d4abb1bd9
himnae18 <himnae18@gmail.com>
2026-02-21 17:37:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-let songs = JSON.parse(localStorage.getItem("jpBright")) || [];
+let songs = JSON.parse(localStorage.getItem("japanBright")) || [];
 
-function addSong(){
-  let title = document.getElementById("title").value;
-  let yt = document.getElementById("yt").value;
-
-  songs.push({title, yt});
-  localStorage.setItem("jpBright", JSON.stringify(songs));
-  location.reload();
+function saveSongs() {
+  localStorage.setItem("japanBright", JSON.stringify(songs));
 }
 
-window.onload = () => {
-  songs.forEach(s=>{
-    let id = s.yt.split("v=")[1];
-    document.getElementById("list").innerHTML += `
+function render() {
+  let html = "";
+  songs.forEach((s) => {
+    html += `
+    <div class="song">
       <h3>${s.title}</h3>
-      <iframe width="300" src="https://www.youtube.com/embed/${id}"></iframe>
-      <hr>
+      <iframe width="400" height="225"
+      src="https://www.youtube.com/embed/${s.yt}"
+      frameborder="0" allowfullscreen></iframe>
+    </div>
+    <hr>
     `;
   });
+  document.getElementById("list").innerHTML = html;
 }
+
+function addSong() {
+  let title = document.getElementById("title").value;
+  let yt = document.getElementById("yt").value.split("v=")[1];
+
+  songs.push({title, yt});
+  saveSongs();
+  render();
+}
+
+render();
